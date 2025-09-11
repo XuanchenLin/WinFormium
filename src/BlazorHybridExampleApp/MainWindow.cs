c04ddaadf6e032067036745467889a4f9102c487
林选臣 <xuanchenlin@msn.com>
2025-09-12 00:03:09 +0800

    ✨ 更新 BlazorHybrid 示例应用，增强 UI 组件支持

--- src/BlazorHybridExampleApp/MainWindow.cs
+++ src/BlazorHybridExampleApp/MainWindow.cs
@@ -5,31 +5,73 @@
 
 using Microsoft.Extensions.FileProviders;
 
+using BlazorHybridExampleApp.Components;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FluentUI.AspNetCore.Components;
+
 using WinFormium;
 using WinFormium.BlazorHybrid;
 
 namespace BlazorHybridExampleApp;
 internal class MainWindow : BlazorFormium
 {
+    BlazorFormiumOptions? _opts;
+
     public MainWindow()
     {
         Url = "https://blazorapp.local/";
 
         WindowTitle = "Blazor Hybrid Example App";
 
-        Icon = new Icon(new MemoryStream(Properties.Resources.WinFormiumBlazor));
+        Icon = new System.Drawing.Icon(new MemoryStream(Properties.Resources.WinFormiumBlazor));
 
-        StaticResources = typeof(MainWindow).Assembly;
 
-        BackColor = Color.Transparent;
+        BackColor = System.Drawing.Color.Transparent;
     }
 
-    public override Type RootComponent => typeof(Counter);
-    public override string BlazorServerAddress => "https://blazorapp.local/";
+    public override BlazorFormiumOptions Options
+    {
+        get
+        {
+            if (_opts == null)
+            {
+                var opts = new BlazorFormiumOptions()
+                {
+                    Scheme = "https",
+                    DomainName = "blazorapp.local",
+                    ConfigureServices = services =>
+                    {
+                        services.AddFluentUIComponents(options =>
+                        {
+                            options.ValidateClassNames = false;
+
+                        });
+
+                        services.AddHttpClient();
+                    },
+                };
+
+                opts.RootComponents.Add<App>("#app");
+                opts.RootComponents.Add<HeadOutlet>("head::after");
+                opts.StaticResources.Add(new BlazorFormiumAssemblyResources(typeof(MainWindow).Assembly));
+
+                _opts = opts;
+            }
+
+            return _opts;
+        }
+    }
+
+
 
     protected override WindowSettings ConfigureWindowSettings(HostWindowBuilder opts)
     {
         var settings = opts.UseDefaultWindow();
+
+        settings.ExtendsContentIntoTitleBar = true;
+
 
         //settings.SystemBackdropType = SystemBackdropType.Transient;
 
